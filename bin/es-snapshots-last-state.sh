4006cff87c3eb236d577c760ed3d1f63275587c0
Damien Vannson <damien@thunken.com>
2021-04-22 18:01:34 +0200

    Scheme option added touching STATUS_TMP_FILE file by default does not trigger an error anymore on rm STATUS_TMP_FILE

--- bin/es-snapshots-last-state.sh
+++ bin/es-snapshots-last-state.sh
@@ -8,17 +8,21 @@ while getopts ":h:p:r:" opt; do
     ;;
     r) ES_REPOSITORY="$OPTARG"
     ;;
+    s) ES_SCHEME="$OPTARG"
+    ;;
     \?) echo "Invalid option -$OPTARG" >&2
     ;;
   esac
 done
 
+ES_SCHEME=${ES_SCHEME:-http}
 ES_HOST=${ES_HOST:-localhost}
 ES_PORT=${ES_PORT:-9200}
 ES_REPOSITORY=${ES_REPOSITORY:-es_repository}
 
 STATUS_TMP_FILE="/tmp/status_contents.tmp"
-STATUS_CODE=$(curl -o ${STATUS_TMP_FILE} -L -s -w "%{http_code}" "${ES_HOST}:${ES_PORT}/_cat/snapshots/${ES_REPOSITORY}?format=JSON&h=id,start_epoch,end_epoch,status,reason&s=start_epoch&pretty")
+touch "/tmp/status_contents.tmp"
+STATUS_CODE=$(curl -o ${STATUS_TMP_FILE} -L -s -w "%{http_code}" "${ES_SCHEME}://${ES_HOST}:${ES_PORT}/_cat/snapshots/${ES_REPOSITORY}?format=JSON&h=id,start_epoch,end_epoch,status,reason&s=start_epoch&pretty")
 if [ ${STATUS_CODE} != "200" ]; then
   echo "Can't get snapshot state. Reason:"
   cat ${STATUS_TMP_FILE}
